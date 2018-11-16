eeb87b90a5c2e990474e9bcee49e4bc60d4aa74e
JeSappelJean <40351638+JeSappelJean@users.noreply.github.com>
2018-11-16 15:00:42 +0100

    MAJ 1 JC

---
+++ vues/vueAccueil.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>PhotoWeb (Test Site)</title>
+        <link rel="stylesheet" type="text/css" href="stylePageAccueil.css">
+        <!--Affichage petit icône en haut de page-->
+        <link rel="shortcut icon" type="image/ico" href="../data/imagesSite/favicon.ico"/>
+
+    </head>
+
+    <a href="https://www.photoweb.fr/livraison-gratuite">
+      <div id="conteneur1" >
+        <div class="element">
+          <img src="image/colis.png" alt="ImageBoitePhotoWeb">
+        </div>
+        <div class="element">
+          <p><b><span class="bleu">ABONNEMENT LIVRAISON PREMIUM :</span> <span class="blanc">  Livraison gratuite et illimitée !   <span class="souligne">> Je découvre</span></span></b></p>
+        </div>
+      </div>
+    </a>
+
+    <div id="conteneur2">
+      <p>
+          <p><a href="https://www.photoweb.fr/espace_contact/espace-contact.asp">ASSISTANCE : <b>0 820 220 108</b></a> - <a href="https://www.photoweb.fr/espace_contact/espace-contact.asp">SUIVI DE COMMANDE : <b>09 69 32 05 36</b></a></p>
+      </p>
+    </div>
+
+    <div id="conteneur3">
+      <div class="element">
+        <p><b>Sourire Garanti</b></p>
+      </div>
+      <div class="element">
+        <img src="image/LogoPhotoweb.png" alt="ImageLogoPhotoWeb">
+      </div>
+    </div>
+
+    <body>
+
+    </body>
+</html>
