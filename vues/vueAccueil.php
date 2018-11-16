067d030e635fd10c496936e2613149044a24fda0
JeSappelJean <40351638+JeSappelJean@users.noreply.github.com>
2018-11-16 15:09:56 +0100

    MAJ 3 JC

--- vues/vueAccueil.php
+++ vues/vueAccueil.php
@@ -12,7 +12,7 @@
     <a href="https://www.photoweb.fr/livraison-gratuite">
       <div id="conteneur1" >
         <div class="element">
-          <img src="imagesSite/colis.png" alt="ImageBoitePhotoWeb">
+          <img src="../data/imagesSite/colis.png" alt="ImageBoitePhotoWeb">
         </div>
         <div class="element">
           <p><b><span class="bleu">ABONNEMENT LIVRAISON PREMIUM :</span> <span class="blanc">  Livraison gratuite et illimitée !   <span class="souligne">> Je découvre</span></span></b></p>
@@ -31,7 +31,7 @@
         <p><b>Sourire Garanti</b></p>
       </div>
       <div class="element">
-        <img src="imagesSite/LogoPhotoweb.png" alt="ImageLogoPhotoWeb">
+        <img src="../data/imagesSite/LogoPhotoweb.png" alt="ImageLogoPhotoWeb">
       </div>
     </div>
 
