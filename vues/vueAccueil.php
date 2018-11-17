8481dd973c3d76f2d79f11dd4dcb744f36f1e8cd
JeSappelJean <40351638+JeSappelJean@users.noreply.github.com>
2018-11-17 12:55:24 +0100

    MAJ 6 JC

--- vues/vueAccueil.php
+++ vues/vueAccueil.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
     <head>
         <meta charset="utf-8" />
         <title>PhotoWeb (Test Site)</title>
@@ -9,33 +10,47 @@
 
     </head>
 
-    <a href="https://www.photoweb.fr/livraison-gratuite">
-      <div id="conteneur1" >
-        <div class="element">
-          <img src="../data/imagesSite/colis.png" alt="ImageBoitePhotoWeb">
-        </div>
-        <div class="element">
-          <p><b><span class="bleu">ABONNEMENT LIVRAISON PREMIUM :</span> <span class="blanc">  Livraison gratuite et illimitée !   <span class="souligne">> Je découvre</span></span></b></p>
-        </div>
-      </div>
-    </a>
-
-    <div id="conteneur2">
-      <p>
-          <p><a href="https://www.photoweb.fr/espace_contact/espace-contact.asp">ASSISTANCE : <b>0 820 220 108</b></a> - <a href="https://www.photoweb.fr/espace_contact/espace-contact.asp">SUIVI DE COMMANDE : <b>09 69 32 05 36</b></a></p>
-      </p>
-    </div>
-
-    <div id="conteneur3">
-      <div class="element">
-        <p><b>Sourire Garanti</b></p>
-      </div>
-      <div class="element">
-        <img src="../data/imagesSite/LogoPhotoweb.png" alt="ImageLogoPhotoWeb">
-      </div>
-    </div>
-
     <body>
 
+      <header>
+
+        <article class="test">
+            <a href="https://www.photoweb.fr/livraison-gratuite">
+                <div>
+                  <img src="../data/imagesSite/colis.png" alt="ImageBoitePhotoWeb">
+                </div>
+
+                <div>
+                  <p><b><span class="bleu">ABONNEMENT LIVRAISON PREMIUM :</span> <span class="blanc">  Livraison gratuite et illimitée !   <span class="souligne">> Je découvre</span></span></b></p>
+                </div>
+            </a>
+        </article>
+
+        <section>
+          <p>
+              <p><a href="https://www.photoweb.fr/espace_contact/espace-contact.asp">ASSISTANCE : <b>0 820 220 108</b></a> - <a href="https://www.photoweb.fr/espace_contact/espace-contact.asp">SUIVI DE COMMANDE : <b>09 69 32 05 36</b></a></p>
+          </p>
+        <section>
+
+        <nav>
+          <div class="element">
+            <p><b>Sourire Garanti</b></p>
+          </div>
+          <div class="element">
+            <img src="../data/imagesSite/LogoPhotoweb.png" alt="ImageLogoPhotoWeb">
+          </div>
+          <div class="element">
+            <section>
+                <p> <a href="https://www.photoweb.fr/moncompte/Account/LogOn?ReturnUrl=%2fmoncompte%2fcreations"> <img src="../data/imagesSite/brushIcon.png" alt="pinceau"> </p>
+                <p> <a href="https://www.photoweb.fr/moncompte/Account/LogOn?ReturnUrl=%2fmoncompte%2fphotos"> <img src="../data/imagesSite/cadreIcon.png" alt="cadre"> </p>
+                <p> <a href="https://www.photoweb.fr/moncompte/Account/LogOn?ReturnUrl=%2fmoncompte%2f"> <img src="../data/imagesSite/userIcon.png" alt="user"> </p>
+                <p> <a href="https://www.photoweb.fr/moncompte/Account/LogOn?ReturnUrl=%2fPanier"> <img src="../data/imagesSite/panierIcon.png" alt="panier"> </p>
+            </section>
+          </div>
+        </nav>
+
+      </header>
+
     </body>
+
 </html>
