872e2f601bc8d066134b1196291d2fa1cdec54ad
DUBESSET Jean Come <dubessej@hostname.iut2.upmf-grenoble.fr>
2018-12-21 15:26:19 +0100

    MAJ jc

--- vues/vueFinalisation.php
+++ vues/vueFinalisation.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8" />
         <title>PhotoWeb (Test Site)</title>
-        <link rel="stylesheet" type="text/css" href="../vues/styleVuePartage.css">
+        <link rel="stylesheet" type="text/css" href="../vues/stylePagePartage.css">
 
         <!--Affichage petit icône en haut de page-->
         <link rel="shortcut icon" type="image/ico" href="../data/imagesSite/favicon.ico"/>
@@ -14,9 +14,22 @@
     <body>
       <!--Affichage du bandeau du site-->
       <?php include_once('../vues/vueBandeauSite.php') ?>
+
+      <!--Affichage Image (Template par la suite)-->
       <footer>
+
+        <nav>
+          <img src="../data/imagesSite/photoImageTest.jpg" alt="ImageTestPartage">
+        </nav>
+
+        <nav>
+          <input type="button" value="Partage">
+          <input type="button" value="Sauvegarde">
+        </nav>
+
       </footer>
-      Vue partage à venir
+
+
     </body>
 
 </html>
