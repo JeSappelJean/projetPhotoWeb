d206fbc36dbbeb92dc8a74ff8ae3f1a71a0f4f07
fontaipi <pierrefontaine00@gmail.com>
2019-01-09 06:41:36 +0100

    Changement des thèmes sans recharher la page

--- vues/vueCreationTemplate.php
+++ vues/vueCreationTemplate.php
@@ -32,7 +32,7 @@
                         <p>num:</p>
                         <input type="text" name="num" required="required">
                         <p>theme:</p>
-                        <input type="text" name="theme" required="required">
+                        <p id="choixTheme">Aucun thème choisi</p>
                         <p>nbpages:</p>
                         <input type="text" name="nbpages" required="required">
                         <input type="submit" value="valider">
@@ -48,11 +48,11 @@
             <div id="column_right">
               <p> <b>Choisissez le thème de votre Template : </b></p>
               <p>
-                <b>
-                  <a id="boutonSaisonsH" href="#">Hiver</a>
-                  <a id="boutonSaisonsA" href="#">Automne</a>
-                  <a id="boutonSaisonsP" href="#">Printemps</a>
-                  <a id="boutonSaisonsE" href="#">Eté</a>
+                <b id = "theme">
+                  <input id="boutonSaisonsH" type="button" value="Hiver"/>
+                  <input id="boutonSaisonsA" type="button" value="Automne"/>
+                  <input id="boutonSaisonsP" type="button" value="Printemps"/>
+                  <input id="boutonSaisonsE" type="button" value="Eté"/>
                 </b>
               </p>
 
@@ -64,6 +64,42 @@
       <nav>
         <a href="../controleur/afficherVueFinalisation.ctrl.php" title="finaliser"><input type="button" value="Finaliser"></a>
       </nav>
+      <script>
+        function creerTemplate(theme){
+          var xhr = new XMLHttpRequest();
+          xhr.open('GET','http://localhost/PhotoShare/vues/vueCreationTemplate.php');
+          xhr.addEventListener('readystatechange',function(){
+            //Vérification que l'événement est terminé (DONE) et que la requète à été résalisé avec succès (statut 200)
+            //Affichage du theme
+              if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
+                document.getElementById('choixTheme').innerHTML = theme ;
+              }
+
+          });
+
+          xhr.send(null);
+        }
+
+        //Attend un clic sur un bouton thème et envoie le theme à la requète XML
+        function selectTheme(){
+          var tableTheme = document.getElementById('theme');
+          var theme = tableTheme.getElementsByTagName("input");
+          var nbTheme = theme.length;
+
+          for(var i = 0; i < nbTheme; i++) {
+              theme[i].addEventListener('click',eventClick,false);
+          }
+        }
+
+        function eventClick(th){
+          creerTemplate(th.target.value);
+        }
+        //Excécuter selectTheme quand le page est complètement chargée
+        window.addEventListener('load',selectTheme);
+
+
+
+      </script>
     </body>
 
 </html>
