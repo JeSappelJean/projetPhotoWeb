3895659a5830f5610eb6bfd6287032b9104df7a9
fontaipi <pierrefontaine00@gmail.com>
2019-01-10 21:50:15 +0100

    bd ok

--- data/insertCompte.sql
+++ data/insertCompte.sql
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 insert into UTILISATEURS values('admin', 'admin');
-=======
-insert into UTILISATEURS values("admin", "admin");
->>>>>>> 8ee8c8933aea80c1b1e782a4860b0ccf9d2d0c1c
--- data/insertLivre.sql
+++ data/insertLivre.sql
@@ -1,2 +1,2 @@
-insert into livre values (1,"PhotoWeb",1);
-insert into livre values (2,"PhotoWeb",2);
+insert into LIVRE values (1,"PhotoWeb",1);
+insert into LIVRE values (2,"PhotoWeb",2);
