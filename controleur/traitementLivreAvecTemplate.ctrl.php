84ba91a84ea816ab540353ccf3e55a728f7c1e19
JeSappelJean <40351638+JeSappelJean@users.noreply.github.com>
2019-01-10 22:07:56 +0100

    maj jc

---
+++ controleur/traitementLivreAvecTemplate.ctrl.php
@@ -0,0 +1,5 @@
+<!--Traitement pour la création d'un nouveau template-->
+<?php
+    session_start();
+    include('../vues/vueCreationLivre.php');
+?>
--- data/insertLivre.sql
+++ data/insertLivre.sql
@@ -1,2 +1,2 @@
-insert into livre values (1,"PhotoWeb",1);
-insert into livre values (2,"PhotoWeb",2);
+insert into Livre values (1,"PhotoWeb",1);
+insert into Livre values (2,"PhotoWeb",2);
