b9bd8c40455e8d09df9951c5ba7bc105d3d8defa
JeSappelJean <40351638+JeSappelJean@users.noreply.github.com>
2019-01-11 18:30:58 +0100

    maj jc

--- vues/vueMonthTheme.php
+++ vues/vueMonthTheme.php
@@ -137,14 +137,6 @@
               </div>
             </div>
 
-
-<div id="resultat"></div>
-
-
-
-              <footer>
-
-      </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     </body>
 
